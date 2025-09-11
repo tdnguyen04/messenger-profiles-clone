ca511f782214db4a1c5aa6797ba5cd4469d159e2
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-11 19:47:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Model
+Goal: Cloning the layout of Messenger
+![alt text](./src/assets/image.png)
+
+## Steps
+
+#### Import Data
+Update README with the final goal and prepare the profiles' images in src/assets. Then prepare data in data.js
+
+
+#### Setup State Variable
+
+
+#### Iterate and Render
+
+
+#### Clear List
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    name: 'tui:>',
+    image: './assets/profile-1.png',
+  },
+  {
+    id: 2,
+    name: 'Huynh Ngoc Bao Tran',
+    image: './assets/profile-2.png',
+  },
+  {
+    id: 3,
+    name: 'Khôi Đào',
+    image: './assets/profile-3.png',
+  },
+  {
+    id: 4,
+    name: 'homiez - 143 Institute Rd',
+    image: './assets/profile-4.png'
+  },
+  {
+    id: 5,
+    name: 'John Red',
+    image: './assets/profile-5.png',
+  },
+];
