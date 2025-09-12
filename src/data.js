0c4c47866ae44f270c36fe98d231e82022c2f633
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-11 21:58:34 -0400

    delete /public from URL for easier deploy on Netlify

--- src/data.js
+++ src/data.js
@@ -2,26 +2,26 @@ export default [
   {
     id: 1,
     name: 'tui:>',
-    image: '/public/assets/profile-1.jpg',
+    image: '/assets/profile-1.jpg',
   },
   {
     id: 2,
     name: 'Huynh Ngoc Bao Tran',
-    image: '/public/assets/profile-2.jpg',
+    image: '/assets/profile-2.jpg',
   },
   {
     id: 3,
     name: 'Khôi Đào',
-    image: '/public/assets/profile-3.jpg',
+    image: '/assets/profile-3.jpg',
   },
   {
     id: 4,
     name: 'homiez - 143 Institute Rd',
-    image: '/public/assets/profile-4.jpg'
+    image: '/assets/profile-4.jpg'
   },
   {
     id: 5,
     name: 'John Red',
-    image: '/public/assets/profile-5.jpg',
+    image: '/assets/profile-5.jpg',
   },
 ];
