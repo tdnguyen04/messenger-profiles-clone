b0970de46cde636f172a909c55541185942b77b1
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-11 21:55:29 -0400

    fix: move all assets to public folder for vite build

--- src/data.js
+++ src/data.js
@@ -2,26 +2,26 @@ export default [
   {
     id: 1,
     name: 'tui:>',
-    image: '/src/assets/profile-1.jpg',
+    image: '/public/assets/profile-1.jpg',
   },
   {
     id: 2,
     name: 'Huynh Ngoc Bao Tran',
-    image: '/src/assets/profile-2.jpg',
+    image: '/public/assets/profile-2.jpg',
   },
   {
     id: 3,
     name: 'Khôi Đào',
-    image: '/src/assets/profile-3.jpg',
+    image: '/public/assets/profile-3.jpg',
   },
   {
     id: 4,
     name: 'homiez - 143 Institute Rd',
-    image: '/src/assets/profile-4.jpg'
+    image: '/public/assets/profile-4.jpg'
   },
   {
     id: 5,
     name: 'John Red',
-    image: '/src/assets/profile-5.jpg',
+    image: '/public/assets/profile-5.jpg',
   },
 ];
