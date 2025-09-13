994ec7fdee83a16c77c00a31b72b1e2da4db457a
tdnguyen04 <tdnguyen04.work@gmail.com>
2025-09-13 11:29:29 -0400

    feat: add ellipsis when hovering over each profile. Functionality to be added later

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,8 +2,13 @@ import React from "react";
 export function Profile({ profile }) {
   return (
     <div className="profile">
-      <img src={profile.image} alt={profile.name} className="avatar" />
-      <h5>{profile.name}</h5>
+      <div className="profile-info">
+        <img src={profile.image} alt={profile.name} className="avatar" />
+        <h5>{profile.name}</h5>
+      </div>
+      <div className="profile-options">
+        <span className="ellipsis-icon"><i class="fa-solid fa-ellipsis"></i></span>
+      </div>
     </div>
   );
 }
